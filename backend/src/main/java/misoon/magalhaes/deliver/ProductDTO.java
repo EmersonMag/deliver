40c492e7998c8f20eafb01cfc1b2d762a63a4546
Emerson da Silva Magalhaes <emerson.mag@hotmail.com>
2021-02-09 10:04:25 -0300

    GET /Products

--- backend/src/main/java/misoon/magalhaes/deliver/ProductDTO.java
+++ backend/src/main/java/misoon/magalhaes/deliver/ProductDTO.java
@@ -1,33 +1,34 @@
 package misoon.magalhaes.deliver;
 
-import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Table(name ="tb_product")
-public class Product implements Serializable {
+public class ProductDTO implements Serializable {
 
-    private  static final long serialversionUID =1L;
+    private static final long serialVersionUID = 1l;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private double price;
     private String description;
     private String imageUri;
 
-    public Product(){
-
+    public ProductDTO(){
     }
 
-    public Product(Long id,String name, Double price,String description,String imageUri){
-        super();
+    public ProductDTO(Long id, String name, double price, String description, String imageUri) {
+
         this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
         this.imageUri = imageUri;
+    }
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        imageUri = entity.getImageUri();
     }
 
     public Long getId() {
@@ -69,30 +70,4 @@ public class Product implements Serializable {
     public void setImageUri(String imageUri) {
         this.imageUri = imageUri;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Product other = (Product) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
 }
