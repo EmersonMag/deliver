1ff9c8758125c87d07504882d47c7e03c54c913e
Emerson da Silva Magalhaes <emerson.mag@hotmail.com>
2021-02-11 14:32:55 -0300

    Post/Orders

--- backend/src/main/java/misoon/magalhaes/deliver/OrderController.java
+++ backend/src/main/java/misoon/magalhaes/deliver/OrderController.java
@@ -2,10 +2,10 @@ package misoon.magalhaes.deliver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -22,4 +22,14 @@ public class OrderController {
         return ResponseEntity.ok().body(list);
 
     }
+    @PostMapping
+    public  ResponseEntity<OrderDTO> insert (@RequestBody OrderDTO dto){
+        dto = service.insert(dto);
+        URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+                return ResponseEntity.created(uri).body(dto);
+    }
+
+
+
 }
